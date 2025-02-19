d22b0da86e006136b9f599535cced4287a1d4e8b
jarthurdev <jarthurdevv@gmail.com>
2025-02-19 16:21:49 -0300

    Adicionando um paciente

--- src/nutritrack/App.java
+++ src/nutritrack/App.java
@@ -3,6 +3,9 @@ package nutritrack;
 public class App {
     public static void main(String[] args) throws Exception {
         Paciente joao = new Paciente("João", 13, 02, 06, "email@email", "masculino");
+        Paciente brunno = new Paciente("Brunno", 01, 8, 05, "email@email", "masculino");
         System.out.println(joao.toString());
+        System.out.println(brunno.toString());
+
     }
 }
